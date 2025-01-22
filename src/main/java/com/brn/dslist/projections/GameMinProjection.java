8d533b4dae8271cb825777bdbe601af5b500cdf7
brunoppivetti <brunopivetti2004@gmail.com>
2025-01-22 17:13:19 -0300

    add GET games by list endpoint

---
+++ src/main/java/com/brn/dslist/projections/GameMinProjection.java
@@ -0,0 +1,11 @@
+package com.brn.dslist.projections;
+
+public interface GameMinProjection {
+
+	Long getId();
+	String getTitle();
+	Integer getYear();
+	String getImgUrl();
+	String getShortDescription();
+	Integer getPosition();
+}
--- src/main/java/com/brn/dslist/services/GameService.java
+++ src/main/java/com/brn/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.brn.dslist.dto.GameDTO;
 import com.brn.dslist.dto.GameMinDTO;
 import com.brn.dslist.entities.Game;
+import com.brn.dslist.projections.GameMinProjection;
 import com.brn.dslist.repositories.GameRepository;
 
 @Service
@@ -27,6 +28,12 @@ public class GameService {
 	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId){
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
 }
